44c1c20001783fde126ee79a0f6054a1ff779f6a
Jonathan Paradiso <33237727+JonPara@users.noreply.github.com>
2018-05-12 20:58:21 -0400

    Add files via upload
    Appended loop properly. Data has been inserted into tables. 
    --Continue to add input tags or drop down for "Date & Time" column.

--- JavaScript_Homework/JavaScript.js
+++ JavaScript_Homework/JavaScript.js
@@ -1,7 +1,5 @@
 // Get references to the tbody element, input field and button
 var $tbody = document.querySelector("tbody");
-var $stateInput = document.querySelector("#state");
-var $searchBtn = document.querySelector("#search");
 
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
 //searchButton.addEventListener("click", handleSearchButtonClick);
@@ -14,24 +12,17 @@ function renderTable() {
   $tbody.innerHTML = "";
   for (var i = 0; i < filteredData.length; i++) {
     // Get get the current address object and its fields
-    var currentData = filteredData[i];
-    var fields = Object.keys(currentData);
+    var data = filteredData[i];
+    var fields = Object.keys(data);
     // Create a new row in the tbody, set the index to be i + startingIndex
-    var $rows = $tbody.insertRow(i);
+    var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
       // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
-    var data_field = $rows[j];
-    var $cell = $rows.insertCell(j);
-    $cell.innerText = ufoData[data_field];
+    var field = fields[j];
+    var $cell = $row.insertCell(j);
+    $cell.innerText = data[field];
     }
   }
 }
-
-function handleSearchButtonClick() {
-  // Format the user's search by removing leading and trailing whitespace, lowercase the string
-  var filterDate = $dateTimeInput.value.trim()};
-
-
-
 
 renderTable();
