9f80c889aabae3ff6f6050e76fbf5a363f351c8a
Jonathan Paradiso <33237727+JonPara@users.noreply.github.com>
2018-05-14 18:15:57 -0400

    Add files via upload
    Added filters, but nothing comes up. Data also does not show up when "search button" is called anywhere in the JS file

--- JavaScript_Homework/JavaScript.js
+++ JavaScript_Homework/JavaScript.js
@@ -5,9 +5,12 @@ var $cityInput = document.querySelector("#city");
 var $stateInput = document.querySelector("#state");
 var $countryInput = document.querySelector("#country");
 var $shapeInput = document.querySelector("#shape");
+var searchBtn = document.querySelector("#search");
+
+
 
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
-//searchButton.addEventListener("click", handleSearchButtonClick);
+//$searchBtn.addEventListener("click", handleSearchButtonClick);
 
 // Set filteredAddresses to addressData initially
 var filteredData = ufoData;
@@ -24,27 +27,67 @@ function renderTable() {
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
       // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
-    var field = fields[j];
-    var $cell = $row.insertCell(j);
-    $cell.innerText = data[field];
+      var field = fields[j];
+      var $cell = $row.insertCell(j);
+      $cell.innerText = data[field];
     }
   }
 }
 //renderTable();
 
-function handleSearchButtonClick() {
-  // Format the user's search by removing leading and trailing whitespace, lowercase the string
-  var filterState = $stateInput.value.trim().toLowerCase();
 
-  // Set filteredAddresses to an array of all addresses whose "state" matches the filter
-  filteredAddresses = addressData.filter(function(address) {
-    var addressState = address.state.toLowerCase();
+
+function handleSearchButtonClick(event) {
+  // prevent page from refreshing
+  event.preventDefault();
+
+  var filterDate = $dateInput.value.trim();
+  if(filterDate != ""){
+    filteredData = filteredData.filter(function(data) {
+      var dataDate = data.datetime
+      return dataDate === filteredDate;
+    });
+  };
+
+  var filterCity = $cityInput.value.trim().toLowerCase();
+  if (filterCity != ""){
+     filteredData = filteredData.filter(function(data) {
+      var cityData = data.city.toLowerCase();
 
     // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
-    return addressState === filterState;
+      return cityData === filterCity;
   });
-  renderTable();
-}
+
+  };
+  var filterState = $stateInput.value.trim().toLowerCase();
+  if (filterState != ""){
+     filteredData = filteredData.filter(function(data) {
+      var stateData = data.state.toLowerCase();
+
+    // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+      return stateData === filterState;
+     });
+
+     var filterCountry = $countryInput.value.trim();
+     if(filterCountry != ""){
+       filteredData = filteredData.filter(function(data) {
+         var dataCountry = data.country.toLowerCase();
+         return dataCountry === filteredCountry;
+       });
+     };
+   
+     var filterShape = $shapeInput.value.trim().toLowerCase();
+     if (filterShape != ""){
+        filteredData = filteredData.filter(function(data) {
+         var shapeData = data.shape.toLowerCase();
+   
+       // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+         return shapeData === filterShape;
+     });
+   
+renderTable();
+
+};
 
 // Render the table for the first time on page load
 renderTable();
